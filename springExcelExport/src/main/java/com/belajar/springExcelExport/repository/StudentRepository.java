1276e9b8264e4f22a126863c57871c9666aabbfa
Singarsa <48761463+Singarsa@users.noreply.github.com>
2022-08-26 09:00:39 +0800

    Delete springExcelExport directory

--- springExcelExport/src/main/java/com/belajar/springExcelExport/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.belajar.springExcelExport.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.belajar.springExcelExport.model.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-
-}
